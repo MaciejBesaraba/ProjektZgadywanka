a7ea2a5678799bb608a5c1a9b92f74132957f4b8
MaciejBesaraba <48643474+MaciejBesaraba@users.noreply.github.com>
2019-06-30 22:23:37 +0200

    Add files via upload

---
+++ ModelGry/Gra.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModelGry
+{
+    public partial class Gra
+    {
+        // subtypes
+        public enum Odp { ZaMalo = -1, Trafiono = 0, ZaDuzo = 1 }
+        public enum StanGry { Trwa, Poddana, Odgadnieta }
+
+
+        // fields
+        public readonly int ZakresOd;
+        public readonly int ZakresDo;
+        private readonly int wylosowana;
+        public int? Wylosowana
+        {
+            get
+            {
+                if (Stan != StanGry.Trwa)
+                    return wylosowana;
+                else
+                    return null;
+            }
+        }
+
+        public StanGry Stan { get; private set; }
+
+        public int LicznikRuchow { get; private set; }  = 0;
+
+        // historia gry: ToDo
+
+
+        //constructors
+        public Gra(int min = 1, int max = 100)
+        {
+            ZakresOd = min;
+            ZakresDo = max;
+
+            wylosowana = Losuj(ZakresOd, ZakresDo);
+            //LicznikRuchow = 0;
+            Stan = StanGry.Trwa;
+            historia = new List<Ruch>();
+        }
+
+        // methods
+        public Odp Ocena(int liczba)
+        {
+            LicznikRuchow++;
+            Odp odp;
+            if (liczba < wylosowana)
+                odp = Odp.ZaMalo;
+            else if (liczba > wylosowana)
+                odp = Odp.ZaDuzo;
+
+            // trafiono
+            Stan = StanGry.Odgadnieta;
+            odp = Odp.Trafiono;
+            historia.Add(new Ruch(liczba, odp));
+
+            return odp;
+        }
+
+        public void Poddaj()
+        {
+            Stan = StanGry.Poddana;
+        }
+
+        // auxiliary methods
+        public static int Losuj(int min = 1, int max = 100)
+        {
+            if (min > max)
+            {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            Random generator = new Random();
+            return generator.Next(min, max + 1);
+        }
+    }
+}
---
+++ UnitTestProjectModelGry/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelGry;
+
+namespace UnitTestProjectModelGry
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(1, 10)]
+        [DataRow(1, 1)]
+        [DataRow(10, 1)]
+        public void Losuj_OK(int x, int y)
+        {
+            //Arrange-Act-Assert = AAA
+
+            //Arrange
+
+            //Act
+            int wylos = Gra.Losuj(x, y);
+
+            //Assert
+            Assert.IsTrue(wylos >= Math.Min(x, y)
+                       && wylos <= Math.Max(x, y));
+        }
+    }
+}
